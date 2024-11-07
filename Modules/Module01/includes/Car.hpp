e7a4960005771cf2ee79e14a97fdbd2002626bf5
Tiago Pereira <tiagosodrepereira@gmail.com>
2024-11-07 16:55:45 +0000

    working on module01

---
+++ Modules/Module01/includes/Car.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Car
+{
+	private:
+		std::string name;
+		unsigned int speed;
+
+	public:
+		/* Orthodox Canonical Form */
+		Car();
+		~Car();
+		Car(const Car& originalCar);
+		Car& operator=(const Car& originalCar);
+		/* ----------------------- */
+		Car(const std::string &name);
+
+		void setName(const std::string &name);
+		void setSpeed(unsigned int speed);
+		const std::string& getName(void);
+		unsigned int getSpeed(void);
+};
---
+++ Modules/Module01/srcs/main.cpp
@@ -0,0 +1,8 @@
+#include "../includes/Car.hpp"
+
+int	main(void)
+{
+	Car car1("Porsche 991 GT3");
+	Car car2("Mclaren P1");
+	Car car3("Ferrari Purosangue");
+}
