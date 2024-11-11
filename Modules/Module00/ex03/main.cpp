c9d2cc3ba8e8deabb7f28fb4d5d01b3de6905ba0
Fle-bihh <xilef.nahibel@gmail.com>
2024-11-11 13:37:39 +0000

    Test file change to trigger action

--- Modules/Module00/ex03/main.cpp
+++ Modules/Module00/ex03/main.cpp
@@ -1,41 +1,42 @@
 #include "Phonebook.hpp"
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 
+//
 void run() {
-    Phonebook phonebook;
-    std::string command;
-    while (true) {
-        std::cout << "Enter command (ADD, SEARCH, REMOVE, BOOKMARK, EXIT): ";
-        std::cin >> command;
+  Phonebook phonebook;
+  std::string command;
+  while (true) {
+    std::cout << "Enter command (ADD, SEARCH, REMOVE, BOOKMARK, EXIT): ";
+    std::cin >> command;
 
-        std::transform(command.begin(), command.end(), command.begin(), ::toupper);
+    std::transform(command.begin(), command.end(), command.begin(), ::toupper);
 
-        if (command == "ADD") {
-            std::string name, phoneNumber, nickname;
-            std::cout << "Enter name: ";
-            std::cin >> name;
-            std::cout << "Enter phone number: ";
-            std::cin >> phoneNumber;
-            std::cout << "Enter nickname: ";
-            std::cin >> nickname;
-            phonebook.addContact(name, phoneNumber, nickname);
-        } else if (command == "SEARCH") {
-            phonebook.searchContacts();
-        } else if (command == "REMOVE") {
-            phonebook.removeContact();
-        } else if (command == "BOOKMARK") {
-            phonebook.listBookmarkedContacts();
-        } else if (command == "EXIT") {
-            std::cout << "Shutting down...\n";
-            break;
-        } else {
-            std::cout << "Invalid command.\n";
-        }
+    if (command == "ADD") {
+      std::string name, phoneNumber, nickname;
+      std::cout << "Enter name: ";
+      std::cin >> name;
+      std::cout << "Enter phone number: ";
+      std::cin >> phoneNumber;
+      std::cout << "Enter nickname: ";
+      std::cin >> nickname;
+      phonebook.addContact(name, phoneNumber, nickname);
+    } else if (command == "SEARCH") {
+      phonebook.searchContacts();
+    } else if (command == "REMOVE") {
+      phonebook.removeContact();
+    } else if (command == "BOOKMARK") {
+      phonebook.listBookmarkedContacts();
+    } else if (command == "EXIT") {
+      std::cout << "Shutting down...\n";
+      break;
+    } else {
+      std::cout << "Invalid command.\n";
     }
+  }
 }
 
 int main() {
-    run();
-    return 0;
+  run();
+  return 0;
 }
