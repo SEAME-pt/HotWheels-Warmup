50ec93f82ead707888e6f7e138985d0612a0aa1c
Fle-bihh <xilef.nahibel@gmail.com>
2024-11-11 10:53:30 +0000

    Add: Tests for Module 00 Exercise 03 / Workflow action

--- Modules/Module00/ex03/Phonebook.cpp
+++ Modules/Module00/ex03/Phonebook.cpp
@@ -1,107 +1,122 @@
-
 #include "Phonebook.hpp"
 #include <iostream>
+#include <fstream>
 
-void Phonebook::addContact(const std::string &name,
-                           const std::string &phoneNumber,
-                           const std::string &nickname) {
-  for (size_t i = 0; i < contacts.size(); ++i) {
-    const Contact &contact = contacts[i];
-    if (contact.getPhoneNumber() == phoneNumber) {
-      std::cout << "Phone number must be unique. Contact not added.\n";
-      return;
-    }
-  }
-  contacts.emplace_back(name, phoneNumber, nickname);
-  std::cout << "Contact added.\n";
+// Constructor that loads responses from file
+Phonebook::Phonebook() {
+    loadResponses("responses.txt");
 }
 
+// Function to load responses from a file into the `responses` map
+void Phonebook::loadResponses(const std::string &filename) {
+    std::ifstream file(filename);
+    std::string line;
+
+    while (std::getline(file, line)) {
+        auto delimiterPos = line.find("=");
+        std::string key = line.substr(0, delimiterPos);
+        std::string value = line.substr(delimiterPos + 1);
+        responses[key] = value;
+    }
+}
+
+// Adds a contact if the phone number is unique
+void Phonebook::addContact(const std::string &name, const std::string &phoneNumber, const std::string &nickname) {
+    for (const auto &contact : contacts) {
+        if (contact.getPhoneNumber() == phoneNumber) {
+            std::cout << "Phone number must be unique. Contact not added.\n";
+            return;
+        }
+    }
+    contacts.emplace_back(name, phoneNumber, nickname);
+    std::cout << responses["ADD_SUCCESS"] << "\n";
+}
+
+// Displays the contact list with indices
 void Phonebook::displayContactList() {
-  for (size_t i = 0; i < contacts.size(); ++i) {
-    std::cout << i << ": " << contacts[i].getName() << " ("
-              << contacts[i].getNickname() << ")\n";
-  }
-}
-
-int Phonebook::findContactIndexByPhoneNumber(const std::string &phoneNumber) {
-  for (size_t i = 0; i < contacts.size(); ++i) {
-    if (contacts[i].getPhoneNumber() == phoneNumber) {
-      return i;
+    for (size_t i = 0; i < contacts.size(); ++i) {
+        std::cout << i << ": " << contacts[i].getName() << " (" << contacts[i].getNickname() << ")\n";
     }
-  }
-  return -1;
 }
 
+// Searches for contacts and optionally bookmarks one
 void Phonebook::searchContacts() {
-  displayContactList();
+    displayContactList();
 
-  int index;
-  std::cout << "Enter contact index to view details: ";
-  std::cin >> index;
-
-  if (index >= 0 && index < contacts.size()) {
-    contacts[index].displayContact();
-    std::cout << "Do you want to bookmark this contact? (y/n): ";
-    char bookmark;
-    std::cin >> bookmark;
-
-    if (bookmark == 'y' || bookmark == 'Y') {
-      contacts[index].setBookmark(true);
-      std::cout << "Contact bookmarked.\n";
-    }
-  } else {
-    std::cout << "Invalid index.\n";
-  }
-}
-
-void Phonebook::removeContact() {
-  std::cout << "Enter 1 to remove by index, or 2 to remove by phone number: ";
-
-  int option;
-  std::cin >> option;
-
-  if (option == 1) {
     int index;
-
-    std::cout << "Enter index: ";
+    std::cout << "Enter contact index to view details: ";
     std::cin >> index;
 
     if (index >= 0 && index < contacts.size()) {
-      contacts.erase(contacts.begin() + index);
-      std::cout << "Contact removed.\n";
-    } else {
-      std::cout << "Invalid index.\n";
-    }
-  } else if (option == 2) {
-    std::string phoneNumber;
-    std::cout << "Enter phone number: ";
-    std::cin >> phoneNumber;
+        contacts[index].displayContact();
+        std::cout << "Do you want to bookmark this contact? (y/n): ";
+        char bookmark;
+        std::cin >> bookmark;
 
-    int index = findContactIndexByPhoneNumber(phoneNumber);
-
-    if (index != -1) {
-      contacts.erase(contacts.begin() + index);
-      std::cout << "Contact removed.\n";
+        if (bookmark == 'y' || bookmark == 'Y') {
+            contacts[index].setBookmark(true);
+            std::cout << responses["BOOKMARK_SUCCESS"] << "\n";
+        }
     } else {
-      std::cout << "Phone number not found.\n";
+        std::cout << "Invalid index.\n";
     }
-  } else {
-    std::cout << "Invalid option.\n";
-  }
 }
 
-void Phonebook::listBookmarkedContacts() {
-  int i = 0;
+// Removes a contact by index or phone number
+void Phonebook::removeContact() {
+    std::cout << "Enter 1 to remove by index, or 2 to remove by phone number: ";
+    int option;
+    std::cin >> option;
 
-  for (size_t i = 0; i < contacts.size(); i++) {
-    const Contact &contact = contacts[i];
-    if (contact.getBookmarkStatus()) {
-      contact.displayContact();
-      i++;
+    if (option == 1) {
+        int index;
+        std::cout << "Enter index: ";
+        std::cin >> index;
+
+        if (index >= 0 && index < contacts.size()) {
+            contacts.erase(contacts.begin() + index);
+            std::cout << responses["REMOVE_SUCCESS"] << "\n";
+        } else {
+            std::cout << "Invalid index.\n";
+        }
+    } else if (option == 2) {
+        std::string phoneNumber;
+        std::cout << "Enter phone number: ";
+        std::cin >> phoneNumber;
+
+        int index = findContactIndexByPhoneNumber(phoneNumber);
+        if (index != -1) {
+            contacts.erase(contacts.begin() + index);
+            std::cout << responses["REMOVE_SUCCESS"] << "\n";
+        } else {
+            std::cout << "Phone number not found.\n";
+        }
+    } else {
+        std::cout << "Invalid option.\n";
     }
-  }
+}
 
-  if (i == 0) {
-    std::cout << "No contacts bookmarked yet.\n";
-  }
+// Finds a contact index by phone number
+int Phonebook::findContactIndexByPhoneNumber(const std::string &phoneNumber) {
+    for (size_t i = 0; i < contacts.size(); ++i) {
+        if (contacts[i].getPhoneNumber() == phoneNumber) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Lists all bookmarked contacts
+void Phonebook::listBookmarkedContacts() {
+    int count = 0;
+    for (const auto &contact : contacts) {
+        if (contact.getBookmarkStatus()) {
+            contact.displayContact();
+            count++;
+        }
+    }
+
+    if (count == 0) {
+        std::cout << responses["EMPTY_LIST"] << "\n";
+    }
 }
