c8239813005da0d9faca2d0ad386d5c37339acf1
Tiago Pereira <tiagosodrepereira@gmail.com>
2024-11-15 10:56:46 +0000

    updating action and running it, to make sure it is working

--- Modules/Module03/ex00/Phonebook/build/Desktop_Qt_6_8_0-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Phonebook_FILE /home/programming-pc/seame/warm-up/Modules/Module03/Phonebook/build/Desktop_Qt_6_8_0-Debug/Phonebook)
-set(__QT_DEPLOY_TARGET_Phonebook_TYPE EXECUTABLE)
--- Modules/Module03/ex00/Phonebook/build/Desktop_Qt_6_8_0-Debug/.qt/deploy_Phonebook_deef9109fc.cmake
+++
@@ -1,8 +0,0 @@
-include(/home/programming-pc/seame/warm-up/Modules/Module03/Phonebook/build/Desktop_Qt_6_8_0-Debug/.qt/QtDeploySupport.cmake)
-include("${CMAKE_CURRENT_LIST_DIR}/Phonebook-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE /home/programming-pc/seame/warm-up/Modules/Module03/Phonebook/build/Desktop_Qt_6_8_0-Debug/Phonebook
-    GENERATE_QT_CONF
-)
